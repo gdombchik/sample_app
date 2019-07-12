910f679d83205933ba9a40ab09a631068c721a21
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-12 16:56:50 -0700

    7.1 Showing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all.order(id: :asc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 end
