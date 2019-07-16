c46060856b4c9d3454dd77deafaf4e6ea98162a8
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-16 19:28:17 -0400

    A test for valid submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,4 +29,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select element, "li", "Password is too short (minimum is 6 characters)"
     end
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+
+    #This simply arranges to follow the redirect after submission, resulting in a rendering of the 'users/show' template.
+    follow_redirect!
+
+    assert_template 'users/show'
+    assert_not flash[:success].empty?
+  end
 end
